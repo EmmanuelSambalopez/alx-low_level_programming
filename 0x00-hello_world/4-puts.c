671ae2c9a9851aeefb26e1abb866510ece00fee8
EmmanuelSambalopez <emmanuel3936@gmail.com>
2022-12-12 18:29:23 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
+            puts("/"programming is like building a multilingual puzzle");
+	    reture (0);
+}

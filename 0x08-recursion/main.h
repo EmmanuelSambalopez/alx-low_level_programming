d3c6346fca91ec57d837445a800a1fba9ce09184
EmmanuelSambalopez <emmanuel3936@gmail.com>
2023-11-11 00:23:42 -0800

    0x08. C - Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -10,5 +11,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-
 #endif
